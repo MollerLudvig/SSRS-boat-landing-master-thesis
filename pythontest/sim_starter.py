d450174be5ecfdf12e1396b775689cc7d1921acc
william <almqvistwilliam@gmail.com>
2025-04-14 10:07:14 +0200

    get sim starter from main

--- pythontest/sim_starter.py
+++ pythontest/sim_starter.py
@@ -1,17 +1,24 @@
-#Connections
-from subprocess import Popen, PIPE
+from subprocess import Popen
 from time import sleep
 
-gazebo_sim = "gz sim island.sdf -r".split()
-command_list_drone = "sim_vehicle.py -v ArduPlane -f gazebo-zephyr --model JSON --map --console --out 0.0.0.0:14551 --out 0.0.0.0:14552 -i0 -L Kattegatt".split()
-command_list_boat = "sim_vehicle.py -v ArduCopter -f gazebo-iris --model JSON --map --console --out 0.0.0.0:14561 --out 0.0.0.0:14562 -i1 -L Kattegatt".split()
-
-Popen(gazebo_sim)
+gazeboSim = Popen("gz sim island.sdf -r".split())
 sleep(1)
-drone_process = Popen(command_list_drone, stdin=PIPE)
-sleep(1)
-boat_process = Popen(command_list_boat, stdin=PIPE)
 
-# Try changing so simulations stay up without while true sleep
-while True:
-    sleep(5)
+droneProcess = Popen(
+    "sim_vehicle.py -v ArduPlane -f gazebo-zephyr --model JSON --map --console "
+    "--out 0.0.0.0:14551 --out 0.0.0.0:14552 -i0 -L Kattegatt".split()
+)
+sleep(1)
+
+boatProcess = Popen(
+    "sim_vehicle.py -v ArduCopter -f gazebo-iris --model JSON --map --console "
+    "--out 0.0.0.0:14561 --out 0.0.0.0:14562 -i1 -L Kattegatt".split()
+)
+
+try:
+    while True:
+        sleep(5)
+except KeyboardInterrupt:
+    gazeboSim.terminate()
+    droneProcess.terminate()
+    boatProcess.terminate()
