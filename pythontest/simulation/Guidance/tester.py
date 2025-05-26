af88f8448cdfc21e0dee0597dea974c145eca37f
william <almqvistwilliam@gmail.com>
2025-05-26 14:01:40 +0200

    Update CSV file path for data loading

--- pythontest/simulation/Guidance/tester.py
+++ pythontest/simulation/Guidance/tester.py
@@ -15,8 +15,8 @@ from coordinate_conv import latlon_to_xy, xy_to_latlon, ned_to_latlon, latlon_to
 # csv_file = "pythontest/Guidance/valo_3.csv"  
 # csv_file = "simulation/Guidance/data_short2_modded.csv"
 # csv_file = "data_short_short.csv"
-# csv_file = "data_short2.csv"
-csv_file = "data_short_OOSM.csv"
+csv_file = "data_short2.csv"
+# csv_file = "data_short_OOSM.csv"
 # csv_file = "ssrs-josephine_1.csv"
 
 df = pd.read_csv(csv_file)
